5df26552bb92f079bfdf51d4180bd6ceef58c72e
HOITMAN <Beatyt3478@student.faytechcc.edu>
2023-08-28 11:48:30 -0400

    Finished

---
+++ M1HW1_Beaty/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+CSC 134
+M1HW1 - Movie Talk
+Beaty
+8/28
+*/
+
+int main()
+{
+    string movie = "The Breakfast Club";
+    int year = 1985;
+    //Box office gross
+    double Box = 51.5;
+
+    cout << movie << "was released in " << year << " and had a box office gross of $ " << Box << " million." << endl;
+    cout << "\"Does Barry Manilow know that you raid his wardrobe?\"" << endl;
+    cout << "\"Answer the question, Claire.\"" << endl;
+    cout << "\"So it's sorta social, demented and sad, but social. Right?\"" << endl;
+    cout << "\"Impossible, sir. It's in Johnson's underwear.\"" << endl;
+    return 0;
+}
